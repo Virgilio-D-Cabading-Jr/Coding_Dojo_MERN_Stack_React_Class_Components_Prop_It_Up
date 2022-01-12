86ec9ad681fe345376f7ab0519d66a9b948d1088
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-12 05:32:44 -0800

    add PersonCard

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Prop It Up</h1>
+      <p>
+        <PersonCard firstName="Jane" lastName="Doe" age={45} hairColor="Black" />
+      </p>
     </div>
   );
 }
