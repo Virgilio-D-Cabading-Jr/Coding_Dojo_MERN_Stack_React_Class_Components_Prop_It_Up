3837873738cb7fabbcbb1c45d5ab1588aa412345
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-12 08:00:52 -0800

    add constructor for PersonCard

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
 
 class PersonCard extends Component {
+    //  //// CONSTRUCTOR //////////////////////////////////////////
+    constructor(props) {
+        super(props);
+        this.firstName = this.props.firstName;
+        this.lastName = this.props.lastName;
+        this.age = this.props.age;
+        this.hairColor = this.props.hairColor;
+    }
+
     render() {
-        const { firstName, lastName, age, hairColor } = this.props;
+
         return (
             <div>
-                <h1>{lastName}, { firstName }</h1>
-                <p>Age: { age }</p>
-                <p>Hair Color: { hairColor }</p>
+                <h1>{this.lastName}, {this.firstName}</h1>
+                <p>Age: {this.age}</p>
+                <p>Hair Color: {this.hairColor}</p>
             </div>
         );
     }
+
+
+
 }
 
 export default PersonCard;
