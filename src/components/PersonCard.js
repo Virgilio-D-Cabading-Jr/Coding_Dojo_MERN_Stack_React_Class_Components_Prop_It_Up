26880c1874027c8fd70d26710bbca5448dc7fba3
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-12 08:47:02 -0800

    implement birthday button in PersonCard

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -4,6 +4,12 @@ class PersonCard extends Component {
     //  //// CONSTRUCTOR //////////////////////////////////////////
     constructor(props) {
         super(props);
+        this.state = {
+            firstName : this.props.firstName,
+            lastName : this.props.lastName,
+            age : this.props.age,
+            hairColor : this.props.hairColor
+        };
         this.firstName = this.props.firstName;
         this.lastName = this.props.lastName;
         this.age = this.props.age;
@@ -14,9 +20,12 @@ class PersonCard extends Component {
 
         return (
             <div>
-                <h1>{this.lastName}, {this.firstName}</h1>
-                <p>Age: {this.age}</p>
-                <p>Hair Color: {this.hairColor}</p>
+                <h1>{this.state.lastName}, {this.state.firstName}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {this.state.hairColor}</p>
+                <button onClick={ () => {
+                    this.setState({ age: (this.state.age+= 1) })
+                    console.log(this.state.age);}}>Birthday Button for {this.state.firstName} {this.state.lastName}</button>
             </div>
         );
     }
