930e6697207186ed82ddf9b9f50d7c37a11c87df
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-12 06:20:56 -0800

    add 3 PersonCards to App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
   return (
     <div className="App">
         <PersonCard firstName="Jane" lastName="Doe" age={45} hairColor="Black" />
+        <PersonCard firstName="John" lastName="Smith" age={88} hairColor="Brown" />
+        <PersonCard firstName="Millard" lastName="Fillmore" age={50} hairColor="Purple" />
+        <PersonCard firstName="Maria" lastName="Smite" age={62} hairColor="Green" />
     </div>
   );
 }
